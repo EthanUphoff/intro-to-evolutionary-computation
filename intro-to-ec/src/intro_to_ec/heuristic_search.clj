fcee31bfae5134696525e0018f12dddb5afbebf0
EthanUphoff <ethan.uphoff@gmail.com>
2019-09-16 15:30:55 -0500

    Start work on heuristic search

---
+++ intro-to-ec/src/intro_to_ec/heuristic_search.clj
@@ -0,0 +1,54 @@
+(ns intro-to-ec.heuristic_search
+  (:require [clojure.set :as cset]
+    [shams.priority-queue :as pq]))
+
+(defn remove-previous-states
+  [new-states frontier visited]
+  (remove (cset/union (set frontier) (set visited)) new-states))
+
+(def depth-first-search
+  {:get-next-node first
+   :add-children concat})
+
+(def breadth-first-search
+  {:get-next-node first
+   :add-children #(concat %2 %1)})
+
+(def random-search
+  {:get-next-node rand-nth
+   :add-children concat})
+
+(defn generate-path
+  [came-from node]
+  (if (= :start-node (get came-from node))
+    [node]
+    (conj (generate-path came-from (get came-from node)) node)))
+
+(defn heuristic 
+  [a, b]
+(+ (Math/abs (- (first a) (first b))) (Math/abs (- (second a) (second b))))
+  )
+;; Priority Queue
+;; https://github.com/shamsimam/clj-priority-queue
+(defn search
+  [{:keys [get-next-node add-children]}
+   {:keys [goal? make-children]}
+   start-node max-calls]
+  (loop [frontier [start-node]
+         came-from {start-node :start-node}
+         num-calls 0]
+    (println num-calls ": " frontier)
+    (println came-from)
+    (let [current-node (get-next-node frontier)]
+      (cond
+        (goal? current-node) (generate-path came-from current-node)
+        (= num-calls max-calls) :max-calls-reached
+        :else
+        (let [kids (remove-previous-states
+                    (make-children current-node) frontier (keys came-from))]
+          (recur
+            (vec (reverse (pq/priority-queue #(heuristic % goal?) :element (add-children
+            kids
+            (rest frontier)))))
+           (reduce (fn [cf child] (assoc cf child current-node)) came-from kids)
+           (inc num-calls)))))))
